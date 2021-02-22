3e5a6eba861b8cb746b6308f72a7741cd08df3d4
Ashiqulalam-Sadi <ashiqul849@gmail.com>
2021-02-22 16:16:58 +0600

    first commit

---
+++ file-handling.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1></h1>
+
+<fieldset>
+              <legend>Basic Information:</legend>
+                
+               
+               <label for="fname">FristName: </label>
+               <input type="text" id="fname" name="fname"><br>
+
+               <label for="lname">LastName: </label>
+               <input type="text" id="lname" name="lname"><br>
+
+               <label for="gender">Gender:</label>
+               <input type="radio" id="male" name="gender" value="male">
+               <label for="male">Male</label>
+               <input type="radio" id="female" name="gender" value="female">
+               <label for="female">Female</label>
+               <input type="radio" id="other" name="gender" value="other">
+               <label for="other">Other</label>
+               <br>
+
+               <label for="email">Email: </label>
+                <input type="text" id="email" name="email"><br>
+                
+            </fieldset>
+
+
+            <fieldset>
+              <legend>User Account Information:</legend>
+                
+               
+               <label for="uname">username/userid: </label>
+               <input type="text" id="uname" name="uname"><br>
+
+               <label for="password">password: </label>
+               <input type="text" id="password" name="password"><br>
+
+               
+
+               <label for="recemail">recovery email address: </label>
+                <input type="text" id="recemail" name="recemail"><br>
+                
+            </fieldset>
+
+
+
